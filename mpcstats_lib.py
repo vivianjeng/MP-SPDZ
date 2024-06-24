0cc81b9edc9461b2d4accf11769c48d5019c9256
mhchia <kevin.mh.chia@gmail.com>
2024-06-24 23:08:21 +0800

    feat: add join
    - Support join with naive for loops
    - Use MAGIC_NUMBER to represent `NULL`
    - Modified mean to support `MAGIC_NUMBER`

--- mpcstats_lib.py
+++ mpcstats_lib.py
@@ -3,26 +3,35 @@ Contains functions can be used in MP-SPDZ circuits.
 """
 
 from Compiler.library import print_ln
-from Compiler.types import sint, Matrix
+from Compiler.types import sint, sfix, Matrix
+from Compiler.util import if_else
 
 
-def read_data(num_parties: int, num_columns: int, num_rows: int) -> list[Matrix]:
+MAGIC_NUMBER = 999
+
+# To enforce round to the nearest integer, instead of probabilistic truncation
+# Ref: https://github.com/data61/MP-SPDZ/blob/e93190f3b72ee2d27837ca1ca6614df6b52ceef2/doc/machine-learning.rst?plain=1#L347-L353
+sfix.round_nearest = True
+
+
+def read_data(party_index: int, num_columns: int, num_rows: int) -> Matrix:
     """
     Read data from each party's input file to a Matrix in MP-SPDZ circuit.
     """
-    data = [Matrix(num_columns, num_rows, sint) for i in range(num_parties)]
+    data = Matrix(num_columns, num_rows, sint)
     # TODO: use @for_range_opt instead?
-    for party_index in range(num_parties):
-        for i in range(num_columns):
-            for j in range(num_rows):
-                data[party_index][i][j] = sint.get_input_from(party_index)
+    for i in range(num_columns):
+        for j in range(num_rows):
+            data[i][j] = sint.get_input_from(party_index)
     return data
 
 
-def print_data(num_columns: int, num_rows: int, data: Matrix):
+def print_data(data: Matrix):
     """
     Print the data in the Matrix.
     """
+    num_columns = data.shape[0]
+    num_rows = data.shape[1]
     for i in range(num_columns):
         for j in range(num_rows):
             print_ln("data[{}][{}]: %s".format(i, j), data[i][j].reveal())
@@ -31,7 +40,9 @@ def print_data(num_columns: int, num_rows: int, data: Matrix):
 # Top 5 functions to implement
 
 def mean(data: list[sint]):
-    return sum(data) / len(data)
+    total = sum(if_else(i != MAGIC_NUMBER, i, 0) for i in data)
+    count = sum(if_else(i != MAGIC_NUMBER, 1, 0) for i in data)
+    return total / count
 
 
 def median(data: list[sint]):
@@ -39,9 +50,62 @@ def median(data: list[sint]):
     raise NotImplementedError
 
 
-def join(data1: list[sint], data2: list[sint]):
-    # TODO: implement join
-    raise NotImplementedError
+def join(data1: Matrix, data2: Matrix, data1_column_index: int, data2_column_index: int) -> Matrix:
+    """
+    Join two matrices based on the matching index in the specified columns.
+
+    :param data1: The first matrix
+    :param data2: The second matrix
+    :param data1_column_index: The column index in data1 to match with data2_column_index
+    :param data2_column_index: The column index in data2 to match with data1_column_index
+
+    For example, if data1 = [
+        [0, 1, 2, 3],
+        [152, 160, 170, 180]
+    ], data2 = [
+        [3, 0, 4],
+        [50, 60, 70],
+    ], data1_column_index = 0, data2_column_index = 0, then the output will be [
+        [0, 1, 2, 3],
+        [152, 160, 170, 180],
+        [0, MAGIC_NUMBER, MAGIC_NUMBER, 3],
+        [60, MAGIC_NUMBER, MAGIC_NUMBER, 50],
+    ]
+    """
+    # E.g. [2, 4]
+    num_columns_1 = data1.shape[0]
+    num_rows_1 = data1.shape[1]
+
+    # E.g. [2, 3]
+    num_columns_2 = data2.shape[0]
+    num_rows_2 = data2.shape[1]
+
+    new_data = Matrix(num_columns_1 + num_columns_2, num_rows_1, sint)
+    # Initialize the first part of the matrix with data1
+    for i in range(num_columns_1):
+        for j in range(num_rows_1):
+            new_data[i][j] = data1[i][j]
+    # Initialize the rest of the matrix with MAGIC_NUMBER
+    for i in range(num_columns_2):
+        for j in range(num_rows_1):
+            new_data[num_columns_2 + i][j] = MAGIC_NUMBER
+
+    # Check the matching index in data1 and data2
+    for i in range(num_rows_1):
+        # Find the corresponding index in data2[data2_column] for data1[data1_column][i]
+        id_in_data1 = data1[data1_column_index][i]
+        for j in range(num_rows_2):
+            # Now checking if data2[data2_column][j] is the same as data1[data1_column][i]
+            id_in_data2 = data2[data2_column_index][j]
+            match = id_in_data1 == id_in_data2
+            # If the match is found, set the entire row of data2[data2_column] to the new_data
+            for k in range(num_columns_2):
+                new_data[num_columns_1 + k][i] = if_else(
+                    match,
+                    data2[k][j],
+                    new_data[num_columns_1 + k][i]
+                )
+    return new_data
 
 
 def covariance(data1: list[sint], data2: list[sint]):
