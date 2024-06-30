83d947250da988566448c6ffe1acd426812ebba3
exfinen <47593166+exfinen@users.noreply.github.com>
2024-06-30 13:03:30 +0900

    remove unnecessary use of @if

--- mpcstats_lib.py
+++ mpcstats_lib.py
@@ -2,7 +2,7 @@
 Contains functions can be used in MP-SPDZ circuits.
 """
 
-from Compiler.library import print_ln, for_range, if_, runtime_error
+from Compiler.library import print_ln, for_range, runtime_error
 from Compiler.types import sint, sfix, Matrix, sfloat, Array
 from Compiler.util import if_else
 from Compiler.mpc_math import sqrt, exp2_fx, log2_fx
@@ -178,13 +178,11 @@ def where(_filter: list[sint], data: list[sint]):
 def geometric_mean(data: list[sint]):
     # check the validity of the dataset
     num_non_positives = sum(if_else(i <= 0, 1, 0) for i in data).reveal()
-    @if_(num_non_positives > 0)
-    def _():
+    if num_non_positives > 0:
         runtime_error('geometric_mean: all numbers in the dataset must be positive')
 
     num_magic_nums = sum(if_else(i == MAGIC_NUMBER, 1, 0) for i in data).reveal()
-    @if_(len(data) == num_magic_nums)
-    def _():
+    if len(data) == num_magic_nums:
         runtime_error('geometric_mean: dataset is empty')
 
     # comupte geometric mean
